7c0fef2492359e3f82528e23ea7c9e12917ff5e4
NotnaKO <anton.kopanov@ya.ru>
2024-03-09 19:33:09 +0300

    Rewrite trees

--- price_retrieval/go.mod
+++ price_retrieval/go.mod
@@ -8,7 +8,14 @@ require (
 )
 
 require (
+<<<<<<< HEAD
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+=======
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+>>>>>>> 46c8d59 (Rewrite trees)
 )
--- price_retrieval/retriever.go
+++ price_retrieval/retriever.go
@@ -1,8 +1,25 @@
-package main
+package price_retrival
 
 type Retriever struct {
 }
 
-func (r *Retriever) Search(info *ConnectionInfo) (float64, error) {
-	return 100, nil
+type searchRequest struct {
+	location *LocationNode
+	category *CategoryNode
+	userID   uint64
+}
+
+// Возвращает цену в копейках
+func (r *Retriever) Search(info *ConnectionInfo) (uint64, error) {
+	location := IDToLocationNodeMap[info.LocationID]
+	category := IDToCategoryNodeMap[info.MicrocategoryID]
+	return r.search(searchRequest{
+		location: location,
+		category: category,
+		userID:   info.UserID,
+	})
+}
+
+func (r *Retriever) search(request searchRequest) (uint64, error) {
+	panic("unimplemented")
 }
