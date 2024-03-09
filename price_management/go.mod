14bb73222ec1751b01b04104e2ab156da01de722
Камиль Якубов <iakubov.kr@phystech.edu>
2024-03-09 17:39:08 +0300

    add HTTP-server for price management service

---
+++ price_management/go.mod
@@ -0,0 +1,10 @@
+module price_management
+
+go 1.22.0
+
+require (
+	github.com/lib/pq v1.10.9
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
---
+++ price_management/http_request_info.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+)
+
+type HttpGetRequestInfo struct {
+	LocationID, MicrocategoryID uint64
+}
+
+func NewGetRequest(r *http.Request) (HttpGetRequestInfo, error) {
+	locationIDStr := r.URL.Query().Get("location_id")
+	if locationIDStr == "" {
+		return HttpGetRequestInfo{}, errors.New("location_id is required")
+	}
+	locationID, err := strconv.ParseUint(locationIDStr, 10, 64)
+	if err != nil {
+		return HttpGetRequestInfo{}, errors.New("invalid location_id")
+	}
+
+	microcategoryIDStr := r.URL.Query().Get("microcategory_id")
+	if microcategoryIDStr == "" {
+		return HttpGetRequestInfo{}, errors.New("microcategory_id is required")
+	}
+	microcategoryID, err := strconv.ParseUint(microcategoryIDStr, 10, 64)
+	if err != nil {
+		return HttpGetRequestInfo{}, errors.New("invalid microcategory_id")
+	}
+
+	return HttpGetRequestInfo{
+		LocationID:      locationID,
+		MicrocategoryID: microcategoryID,
+	}, nil
+}
+
+type HttpSetRequestInfo struct {
+	LocationID, MicrocategoryID uint64
+	Price                       float64
+}
+
+func NewSetRequest(r *http.Request) (HttpSetRequestInfo, error) {
+	locationIDStr := r.URL.Query().Get("location_id")
+	if locationIDStr == "" {
+		return HttpSetRequestInfo{}, errors.New("location_id is required")
+	}
+	locationID, err := strconv.ParseUint(locationIDStr, 10, 64)
+	if err != nil {
+		return HttpSetRequestInfo{}, errors.New("invalid location_id")
+	}
+
+	microcategoryIDStr := r.URL.Query().Get("microcategory_id")
+	if microcategoryIDStr == "" {
+		return HttpSetRequestInfo{}, errors.New("microcategory_id is required")
+	}
+	microcategoryID, err := strconv.ParseUint(microcategoryIDStr, 10, 64)
+	if err != nil {
+		return HttpSetRequestInfo{}, errors.New("invalid microcategory_id")
+	}
+
+	priceStr := r.URL.Query().Get("price")
+	if priceStr == "" {
+		return HttpSetRequestInfo{}, errors.New("price is required")
+	}
+	price, err := strconv.ParseFloat(priceStr, 64)
+	if err != nil {
+		return HttpSetRequestInfo{}, errors.New("invalid microcategory_id")
+	}
+
+	return HttpSetRequestInfo{
+		LocationID:      locationID,
+		MicrocategoryID: microcategoryID,
+		Price:           price,
+	}, nil
+}
