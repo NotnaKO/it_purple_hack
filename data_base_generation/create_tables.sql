1c49fe8f95e61b206655f516677d1a244885fbe1
mr.kustik1985 <gena@2647323-cz17762.twc1.net>
2024-03-10 16:11:01 +0300

    generate tables

--- data_base_generation/create_tables.sql
+++ data_base_generation/create_tables.sql
@@ -1,37 +1,43 @@
 DROP SCHEMA IF EXISTS matrix CASCADE;
 CREATE SCHEMA matrix;
 create table matrix.baseline_matrix_1(
-    microcategory_id int,
-    location_id int,
-    price int
+    microcategory_id int NOT NULL,
+    location_id int NOT NULL,
+    price int,
+    CONSTRAINT nm1 PRIMARY KEY (microcategory_id, location_id)
 );
 
 create table matrix.baseline_matrix_2(
-    microcategory_id int,
-    location_id int,
-    price int
+    microcategory_id int NOT NULL,
+    location_id int NOT NULL,
+    price int,
+    CONSTRAINT nm2 PRIMARY KEY (microcategory_id, location_id)
 );
 
 create table matrix.baseline_matrix_3(
-    microcategory_id int,
-    location_id int,
-    price int
+    microcategory_id int NOT NULL,
+    location_id int NOT NULL,
+    price int,
+    CONSTRAINT nm3 PRIMARY KEY (microcategory_id, location_id)
 );
 
 create table matrix.discount_matrix_1(
-    microcategory_id int,
-    location_id int,
-    price int
+    microcategory_id int NOT NULL,
+    location_id int NOT NULL,
+    price int,
+    CONSTRAINT nm4 PRIMARY KEY (microcategory_id, location_id)
 );
 
 create table matrix.discount_matrix_2(
-    microcategory_id int,
-    location_id int,
-    price int
+    microcategory_id int NOT NULL,
+    location_id int NOT NULL,
+    price int,
+    CONSTRAINT nm5 PRIMARY KEY (microcategory_id, location_id)
 );
 
-create table matrix.discount_matrix_3(
-     microcategory_id int,
-     location_id int,
-     price int
+CREATE TABLE matrix.discount_matrix_3 (
+    microcategory_id int NOT NULL,
+    location_id int NOT NULL,
+    price int,
+    CONSTRAINT nm6 PRIMARY KEY (microcategory_id, location_id)
 );
