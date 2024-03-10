91c0f4672d56273a56926bc0c0a99ab196b76c66
mr.kustik1985 <gena@2647323-cz17762.twc1.net>
2024-03-10 13:11:48 +0300

    insert into tables

---
+++ data_base_generation/discount_matrix_2.sql
@@ -0,0 +1,8 @@
+create table discount_matrix_2(
+    microcategory_id int,
+    location_id int,
+    price int
+);
+
+insert into discount_matrix_2 (microcategory_id, location_id, price)
+values  (18, 20, 216);
---
+++ data_base_generation/discount_matrix_3.sql
@@ -0,0 +1,8 @@
+create table discount_matrix_3(
+     microcategory_id int,
+     location_id int,
+     price int
+);
+
+insert into discount_matrix_3 (microcategory_id, location_id, price)
+values  (1, 1, 1);
