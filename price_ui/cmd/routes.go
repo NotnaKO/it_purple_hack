9c1e0cad3abb06e3a8da68414675d074dc0d03bb
Igor Shalygin <igor@Igors-MacBook-Air.local>
2024-03-11 17:36:20 +0300

    Update website

--- price_ui/cmd/routes.go
+++ price_ui/cmd/routes.go
@@ -8,8 +8,11 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", app.handleSimpleRequest("index.tmpl", "/"))
-	mux.HandleFunc("/save", app.handleStorageRequest)
+	mux.HandleFunc("/storage", app.handleStorageRequest)
+	mux.HandleFunc("/update", app.handleUpdateRequest)
 	mux.HandleFunc("/metrics", app.handleMetricsRequest)
+	mux.HandleFunc("/add_request", app.addRequestHandler)
+	mux.HandleFunc("/get_request", app.getRequestHandler)
 
 	fileServer := http.FileServer(http.Dir("../src/"))
 	mux.Handle("/src", http.NotFoundHandler())
