8fdbd5a760202646c8f77ed0b124064b13c4170d
g3n4ik <mr.kustik1985@gmail.com>
2024-03-13 22:40:00 +0300

    for test

--- data_base_generation/insert_values.sh
+++ data_base_generation/insert_values.sh
@@ -2,6 +2,6 @@
 sudo -u postgres psql -d postgres -a -f baseline_matrix_1.sql #Ubuntu only
 sudo -u postgres psql -d postgres -a -f baseline_matrix_2.sql
 sudo -u postgres psql -d postgres -a -f baseline_matrix_3.sql
-# sudo -u postgres psql -d postgres -a -f discount_matrix_1.sql
-# sudo -u postgres psql -d postgres -a -f discount_matrix_2.sql
-# sudo -u postgres psql -d postgres -a -f discount_matrix_3.sql
+sudo -u postgres psql -d postgres -a -f discount_matrix_1.sql
+sudo -u postgres psql -d postgres -a -f discount_matrix_2.sql
+sudo -u postgres psql -d postgres -a -f discount_matrix_3.sql
